c142c77a3b1563128f671ed0034f6b3fd319292a
auturnn <acki@kakao.com>
2022-05-02 07:32:16 +0900

    GraphQL 기본API구성연습 끝

--- graphql/db.js
+++ graphql/db.js
@@ -1,19 +1,47 @@
-export const people = [
-  {
-    id: 1,
-    name: "didle_didle",
-    age: 28,
-    gender: "male",
-  },
-  {
-    id: 2,
-    name: "HI",
-    age: 12,
-    gender: "male",
-  },
-];
+import axios from "axios";
 
-export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+const BASE_URL = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
+
+//node에서 fetch를 할땐 node-fetch 패키지가 필요
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
+};
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
+};
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movies;
 };
